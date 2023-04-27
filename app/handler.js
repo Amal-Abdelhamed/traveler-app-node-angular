bae3071953af5f42fe1d039da1dbd33c5d37f43b
Amal-Abdelhamed <amalabdelhameed90amalabdelhame@gmail.com>
2023-04-27 23:19:36 +0200

    creat app structur

---
+++ app/handler.js
@@ -0,0 +1,6 @@
+class Handler {
+    static resHandler = (res, statusCode, apiStatus, data, message) => {
+        res.status(statusCode).send({ apiStatus, data, message })
+    }
+}
+module.exports = Handler
