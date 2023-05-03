f1378e7d809e4d8df8859e9ee3db57e890720c2e
Amal-Abdelhamed <amalabdelhameed90amalabdelhame@gmail.com>
2023-05-03 20:58:57 +0300

    auth for user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
-const router=require('express').Router()
-const userController =require("../app/controller/user.controller")
+const router = require('express').Router()
+const userController = require("../app/controller/user.controller")
+const auth = require('../app/middleware/middleware')
 
-router.post("/register",userController.register)
- router.post('/login',userController.login)
-router.post('/logout',userController.logout)
+router.post("/register", userController.register)
+router.post('/login', auth, userController.login)
+router.post('/logout', auth, userController.logout)
 
 
-module.exports=router
+module.exports = router
