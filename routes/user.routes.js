97f55d38fdbdd4b0b244c1b5ea59aa1b96118266
Amal-Abdelhamed <amalabdelhameed90amalabdelhame@gmail.com>
2023-05-03 21:16:34 +0300

    add user role

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const userController = require("../app/controller/user.controller")
-const auth = require('../app/middleware/middleware')
+const auth = require('../app/middleware/user.middleware')
 
 router.post("/register", userController.register)
 router.post('/login', auth, userController.login)
