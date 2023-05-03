57262325e943f341d174c0c077ee40d85c03b5bf
Amal-Abdelhamed <amalabdelhameed90amalabdelhame@gmail.com>
2023-05-03 22:53:44 +0300

    add auth admin logic

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const userController = require("../app/controller/user.controller")
 const auth = require('../app/middleware/user.middleware')
+const authAdmin = require('../app/middleware/admin.middleware')
 
 router.post("/register", userController.register)
-router.post('/login', auth, userController.login)
+router.post('/login', userController.login)
 router.post('/logout', auth, userController.logout)
 
 
