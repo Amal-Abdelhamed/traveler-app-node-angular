a37e1fc52ae7c30fe9a6ac52110a753eba02d20e
maitaha179 <maimtaha96@gmail.com>
2023-05-07 01:09:35 +0200

    destination module

--- app/app.js
+++ app/app.js
@@ -6,8 +6,6 @@ require("../database/connection")
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/api/destination",destinationRoutes)
-app.all("*",async(req,res)=>{
-    res.status(404).send({data:"invalid url"})
-})
+
 module.exports = app
 
