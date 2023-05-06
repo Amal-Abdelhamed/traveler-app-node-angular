bfece5a33799be6fef797f7dbf897c6847a0ba40
maitaha179 <maimtaha96@gmail.com>
2023-05-07 01:01:41 +0200

    destination module

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const app = express()
+const destinationRoutes = require ("../routes/destination.routes")
 require("../database/connection")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-const userRouter=require('../routes/user.routes')
-app.use('/api/user',userRouter)
-
+app.use("/api/destination",destinationRoutes)
+app.all("*",async(req,res)=>{
+    res.status(404).send({data:"invalid url"})
+})
 module.exports = app
 
