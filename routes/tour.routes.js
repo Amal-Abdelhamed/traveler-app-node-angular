dc3362b48a1cb98801514f9d3c4a5af6bf2f1571
eslam <eslam.et2@gmail.com>
2023-05-07 19:52:45 +0300

    collection of tour

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -1,4 +1,12 @@
-const tourController = require('../app/controller/tour.controller')
 const Router = require('express').Router()
+const tourController = require('../app/controller/tour.controller')
+const upload = require("../app/middleWare/upload")
 
-Router.post('addTour', tourController.addTour)
+Router.post('/addTour', tourController.addTour)
+Router.patch('/editTour', tourController.editTour)
+Router.delete('/deleteTour', tourController.deleteOneTour)
+Router.get('/allTour', tourController.allTour)
+Router.delete('/delAllTour', tourController.delAllTour)
+Router.patch("/updatePImg", upload.single("img"), tourController.updatePimg)
+
+module.exports = Router
